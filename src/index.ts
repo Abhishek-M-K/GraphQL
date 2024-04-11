25d2760c69193305582b7cdd4664dce0c81b78b0
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-04-11 19:24:09 +0530

    chore : pg docker and prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db';
 
 async function startApolloServer() {   
     const PORT = Number(process.env.PORT) || 8000;
@@ -15,13 +16,43 @@ async function startApolloServer() {
             type Query {
                 hello: String
             }
+            type Mutation {
+                createUser(firstName: String!, lastName: String!, email:String!, password:String!): Boolean
+            }
         `,
         resolvers: {
             Query: {
                 hello: () => 'Hello , I am a GraphQL Server!'
-            }
-        }
-    })
+            },
+            Mutation: {
+                createUser: 
+                    async (_, 
+                        { 
+                            firstName, 
+                            lastName, 
+                            email, 
+                            password 
+                        }:{ 
+                            firstName: string; 
+                            lastName: string; 
+                            email:string; 
+                            password:string
+                        }) => {
+                    console.log(firstName, lastName, email, password);
+                    await prismaClient.user.create({
+                        data:{
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            salt: process.env.RANDOM_SALT!,
+                        }
+                    })
+                    return true;
+                }
+            },  
+        },
+    });
 
     await graphqlServer.start();
 
